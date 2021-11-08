273fe770daf8140b6514fb883b7d40574760d2a5
AlanBen <abseralvo@gmail.com>
2021-11-08 11:30:03 +0200

    readme

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Reproduction steps:
 * cd functions
 * npm install
 * npm run serve
-* hit the 
+* hit the function endpoint http://localhost:5099/fbdb-test-001/us-central1/helloWorld
--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,7 @@
 const functions = require("firebase-functions");
 const admin = require('firebase-admin')
 
-exports.helloWorld = functions.https.onRequest(async (request, response) => {
+exports.helloWorld = functions.https.onRequest(async (req, resp) => {
 	functions.logger.info("Hello logs!", {structuredData: true});
 
 	const app = admin.initializeApp();
@@ -11,5 +11,5 @@ exports.helloWorld = functions.https.onRequest(async (request, response) => {
 	const v = await db.ref('/test').once('value')
 	console.log("The value is:", v.val())
 
-	response.send("Hello from IR!");
+	resp.send( "Hello from IR!");
 });
